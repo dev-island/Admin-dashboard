2186ca2d5bcf1e44ab82c0af554edb37698fbba0
Andrew Locke <andrew.locke@spiral.us>
2023-09-05 18:20:06 -1000

    remove console.log

--- solution/index.js
+++ solution/index.js
@@ -1,6 +1,5 @@
 const navBtn = document.getElementById("nav-btn");
 const menu = document.getElementById("menu");
-console.log("menu", menu);
 
 function toggleMenu() {
   menu.classList.toggle("active");
