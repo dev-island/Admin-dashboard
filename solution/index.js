639bf63dcf7db52ed2627cb815ddb1a1688f9afd
Andrew Locke <andrew.locke@spiral.us>
2023-09-05 16:43:52 -1000

    init commit

---
+++ solution/index.js
@@ -0,0 +1,7 @@
+const navBtn = document.getElementById("nav-btn");
+const menu = document.getElementById("menu");
+console.log("menu", menu);
+
+function toggleMenu() {
+  menu.classList.toggle("active");
+}
